4283e67d67f6a3fbd0faf1f53cb9996f2d5edf1b
DawsonFuhr <95586105+DawsonFuhr@users.noreply.github.com>
2023-06-26 19:48:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 song = "";
+song2 = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+song1_status = "";
+song2_status = "";
+
 function preload()
 {
 	song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3")
 }
 
 function setup() {
@@ -15,9 +21,8 @@ function setup() {
 	video = createCapture(VIDEO);
 	video.hide();
 
-	poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
-
 }
 
 function modelLoaded()
@@ -28,43 +33,49 @@ function modelLoaded()
 function draw() {
 	image(video, 0, 0, 600, 500);
 
+	song_status = song.isPlaying();
+	
+
 	fill("#FF0000");
 	stroke("#FF0000");
-
+	
+	if(song_status == false)
+		{
+		
+			song.play();
+			
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+			alert(alert);
 	if(scoreLeftWrist > 0.2)
 	{
-	circle(leftWristX, leftWristY,20);
-	InNumberleftWristY = Number(leftwristY);
-	remove_demicals = floor(InNumberleftWristY);
-	volume = remove_demicals/500;
-	document.getElementById("volume").innerHTML = "Volume = " + volume;
-	song.setVolume(volume);
+		circle(leftWristX,leftWristY,20);
+			song.stop();
+
+		
+		}
+		
 	}
 }
 
-function play()
-{
-	song.play();
-	song.setVolume(1);
-	song.rate(1);
 
-}
 
-function gotPoses()
+
+function gotPoses(results)
 {
 	if(results.length > 0)
-	{
-		console.log(results);
-		scoreLeftWrist = reuslts[0].pose.keypoints[9].score;
-		console.log("scoreLeftWrist = " + scoreLeftWrist);
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
 
-
-		leftWristX = results[0].pose.leftWrist.x;
-		leftWristY = results[0].pose.leftWrist.y;
-		console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
-
-		rightWristX = results[0].pose.leftWrist.x;
-		rightWristY = results[0].pose.leftWrist.y;
-		console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY)
-	}
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+}
 }
